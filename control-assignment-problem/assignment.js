2eeb6e0d98d4721e7cad8d16617ed25c5ff74d47
Kaigh Taylor <coders@kai-deas.com>
2022-08-19 15:23:11 -0400

    JSCG | Edited Assignment

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -1,16 +1,16 @@
-for (i=0; i<5; i++) {
+for (i = 0; i < 5; i++) {
   const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
   console.log(randomNumber);
-  if(randomNumber > 0.7) {
+  if (randomNumber > 0.7) {
     alert("randomNumber is greater than 0.7");
   }
 }
 
-var arry = [1,2,3,4,5];
-for (i=0; i<arry.length; i++) {
+var arry = [1, 2, 3, 4, 5];
+for (i = 0; i < arry.length; i++) {
   console.log(arry[i]);
 }
-for (i=arry.length; i>=0; i--) {
+for (i = arry.length; i >= 0; i--) {
   console.log(arry[i]);
 }
 
@@ -20,8 +20,8 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 console.log(randomNumber);
 console.log(rndmNumber);
 
-if((randomNumber > 0.7) && (rndmNumber > 0.7)) {
+if ((randomNumber > 0.7) && (rndmNumber > 0.7)) {
   alert("Both random numbers are greater than 0.7");
-} else if((randomNumber < 0.7) || (rndmNumber < 0.7) ){
-  alert("At least one random number is less than 0.7");
+} else if ((randomNumber <= 0.2) || (rndmNumber <= 0.2)) {
+  alert("At least one random number is less than 0.2");
 }
