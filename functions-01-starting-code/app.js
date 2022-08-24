3131ad96e149e1814c325b680f9cb3a5b72c2f62
Kaigh Taylor <coders@kai-deas.com>
2022-08-24 12:34:07 -0400

    JSCG | functions & Rock Paper Scissors Kaigh Ver

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -6,9 +6,16 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WINS!';
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS!';
+let PLAYER_SELECTION = null;
+let COMPUTER_SELECTION = null;
+
+let gameIsRunning = false;
 
 const getPlayerChoice = function() {
-  const selection = prompt (`${ROCK}, ${PAPER}, or ${SCISSORS}`, '').toUpperCase;
+  const selection = prompt (`${ROCK}, ${PAPER}, or ${SCISSORS}?`, '').toUpperCase();
   if (
     selection !== ROCK &&
     selection !== PAPER &&
@@ -19,8 +26,45 @@ const getPlayerChoice = function() {
   }
   return selection;
 };
+
+
+const getComputerChoice = function() {
+  randomChoice = Math.random();
+  if (randomChoice < 0.34) {
+    return ROCK;
+  } else if (randomChoice < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if(cChoice === ROCK && pChoice === SCISSORS) {
+    return RESULT_COMPUTER_WINS;
+  } else if (cChoice === PAPER && pChoice === ROCK) {;
+    return RESULT_COMPUTER_WINS;
+  } else if (cChoice === SCISSORS && pChoice === PAPER) {;
+    return RESULT_COMPUTER_WINS;
+  } else {
+    return 'Player Wins!';
+  }
+  gameIsRunning = false;
+};
+
+
+
 startGameBtn.addEventListener('click', function startGame() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  PLAYER_SELECTION = getPlayerChoice();
+  console.log(`Player chose ${PLAYER_SELECTION}`);
+  COMPUTER_SELECTION = getComputerChoice();
+  console.log(`Computer chose ${COMPUTER_SELECTION}`);
+  console.log(getWinner(COMPUTER_SELECTION, PLAYER_SELECTION));
 }); 
