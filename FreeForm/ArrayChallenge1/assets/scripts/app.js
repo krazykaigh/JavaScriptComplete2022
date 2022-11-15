568a2fb950c53576d3adb6432c08692af667e09b
Kaigh Taylor <coders@kai-deas.com>
2022-11-15 15:54:34 -0500

    jscg | array challenge 1

---
+++ FreeForm/ArrayChallenge1/assets/scripts/app.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 8*/
+const list = [91, 2, 17, 51, 54, 39, 34, 61, 34, 91];
+let idx1 = 0;
+let idx2 = 1;
+let idx3 = null;
+if (list[idx2] > list[idx1]) {
+idx1 = 1;
+idx2 = 0;
+}
+for (let i = 2; i < list.length; i++) {
+if (list[i] >= list[idx1]) {
+idx3 = idx2;
+idx2 = idx1;
+idx1 = i;
+}
+else if (list[i] >= list[idx2]) {
+idx3 = idx2;
+idx2 = i;
+}
+else if (idx3 === null || list[i] >= list[idx3]) {
+idx3 = i;
+}
+}
+console.log(idx3);
--- FreeForm/FreeFormArrays/assets/scripts/app.js
+++ FreeForm/FreeFormArrays/assets/scripts/app.js
@@ -1,6 +1,8 @@
-/*jshint esversion: 8 */
-const numbers = [1, 2, 3];
-console.log(numbers);
+/*jshint esversion: 8*/
+
+// Typical way to create an array
+/* const numbers = [1, 2, 3];
+console.log(numbers); */
 
 /* const moreNumbers = new Array(5);
 console.log(moreNumbers);
@@ -8,11 +10,43 @@ console.log(moreNumbers);
 const yetMoreNumbers = Array.of(1, 2);
 console.log(yetMoreNumbers); */
 
-const moreNumbers = Array.from('Hello There!');
+/* const moreNumbers = Array.from('Hello There!');
 console.log(moreNumbers);
 
 const listItems = document.querySelectorAll('li');
-console.log(listItems);
+console.log(listItems); */
 
-const arrayListItems = Array.from(listItems);
+//Converts the listItems into an array
+/* const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
+
+const hobbies = ['Cooking', 'Sports'];
+const personalData = [30, 'Max', {moreDetail: []}];
+
+const analyticsData = [[1, 1.6], [-5, 4, 2.1]];
+
+for (const data of analyticsData) {
+    for (const dataPoint of data) {
+        console.log(dataPoint);
+    }
+} */
+
+const hobbies = ['Sports', 'Cooking'];
+// Add item to the end of the array
+hobbies.push('Reading');
+// Add item at the beginning of the array
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+/* hobbies.shift();
+hobbies[1] = 'Coding';
+hobbies[5] = 'Reading'; */
+console.log(hobbies);
+console.log(poppedValue, hobbies);
+
+// Splice method
+hobbies.splice(0, 0, 'Good Food');
+console.log(hobbies);
+
+// Slice method
+const removedElements = hobbies.slice(0, 1);  
+console.log(removedElements);
