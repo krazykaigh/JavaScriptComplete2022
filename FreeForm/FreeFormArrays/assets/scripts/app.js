8b0a623e98f1c5c254f1d3658bb23d6eba3338e4
Kaigh Taylor <coders@kai-deas.com>
2022-11-30 14:38:53 -0500

    JSCG | Array methods fini

--- FreeForm/FreeFormArrays/assets/scripts/app.js
+++ FreeForm/FreeFormArrays/assets/scripts/app.js
@@ -1,123 +1,169 @@
 /*jshint esversion: 8 */
 
 // Typical way to create an array
-/* const numbers = [1, 2, 3];
-console.log(numbers); */
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-/* const moreNumbers = new Array(5);
-console.log(moreNumbers);
+// const moreNumbers = new Array(5);
+// console.log(moreNumbers);
 
-const yetMoreNumbers = Array.of(1, 2);
-console.log(yetMoreNumbers); */
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
 
-/* const moreNumbers = Array.from('Hello There!');
-console.log(moreNumbers);
+// const moreNumbers = Array.from('Hello There!');
+// console.log(moreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems); */
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
 //Converts the listItems into an array
-/* const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ['Cooking', 'Sports'];
-const personalData = [30, 'Max', {moreDetail: []}];
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
 
-const analyticsData = [[1, 1.6], [-5, 4, 2.1]];
+// const analyticsData = [[1, 1.6], [-5, 4, 2.1]];
 
-for (const data of analyticsData) {
-    for (const dataPoint of data) {
-        console.log(dataPoint);
-    }
-} */
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
 
-/* const hobbies = ['Sports', 'Cooking'];
-console.log(hobbies);
-// Add item to the end of the array
-hobbies.push('Reading');
-// Add item at the beginning of the array
-hobbies.unshift('Coding');
-console.log(hobbies);
+// const hobbies = ['Sports', 'Cooking'];
+// console.log(hobbies);
+// // Add item to the end of the array
+// hobbies.push('Reading');
+// // Add item at the beginning of the array
+// hobbies.unshift('Coding');
+// console.log(hobbies);
 
-const poppedValue = hobbies.pop(); */
-/* hobbies.shift();
-hobbies[1] = 'Coding';
-hobbies[5] = 'Reading'; */
-/* console.log(hobbies);
-console.log(poppedValue, hobbies); */
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading';
+// console.log(hobbies);
+// console.log(poppedValue, hobbies);
 
 // Splice method
-/* hobbies.splice(1, 0, 'Good Food');
-console.log(hobbies); */
+// hobbies.splice(1, 0, 'Good Food');
+// console.log(hobbies);
 
 // Slice method
-/* const removedElements = hobbies.splice(-2, 1);  
-console.log(removedElements);
-console.log(hobbies); */
+// const removedElements = hobbies.splice(-2, 1);  
+// console.log(removedElements);
+// console.log(hobbies);
 // const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
 // const storedResults = testResults.slice(2);
-/* const storedResults = testResults.concat([3.99, 2]);
-testResults.push(5.91);
+// const storedResults = testResults.concat([3.99, 2]);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
+// console.log(storedResults, testResults);
 
-console.log(testResults.indexOf(1.5)); */
+// console.log(testResults.indexOf(1.5));
 // create array of names
 // Get index of object with specific value in array
-const needle = 3; // needle
-const haystack = [{ id: 1 }, { id: 2 }, { id: 3 }]; // haystack
-const index = haystack.findIndex((item) => item.id === needle);
-console.log("The value of Needle as found at index: " + index); // 2
+// const needle = 3; // needle
+// const haystack = [{ id: 1 }, { id: 2 }, { id: 3 }]; // haystack
+// const index = haystack.findIndex((item) => item.id === needle);
+// console.log("The value of Needle as found at index: " + index); // 2
 
-const personalData = [
-  { name: "Maxine" },
-  { name: "Manuel" },
-  { name: "Beatrice" },
-  { name: "Lionel" },
-];
-// The following statement will return -1 indicating that the value is not found
-console.log(
-  "Manuel was found at index: " + personalData.indexOf({ name: "Manuel"})
-);
+// const personalData = [
+//   { name: "Maxine" },
+//   { name: "Manuel" },
+//   { name: "Beatrice" },
+//   { name: "Lionel" },
+// ];
+// // The following statement will return -1 indicating that the value is not found
+//  console.log(
+//   "Manuel was found at index: " + personalData.indexOf({ name: "Manuel"})
+// );
 
-const manuel = personalData.find((person, idx, persons) => {
-  return person.name === "Manuel";
-});
-console.log("This is the Manuel object" + JSON.stringify(manuel));
-manuel.name = "Anna";
-console.log("Just changed Manuel to Anna");
-console.log(JSON.stringify(manuel)+ '\nAnd here\'s the array' +JSON.stringify(personalData));
+// const manuel = personalData.find((person, idx, persons) => {
+//   return person.name === "Manuel";
+// });
+// console.log("This is the Manuel object" + JSON.stringify(manuel));
+// manuel.name = "Anna";
+// console.log("Just changed Manuel to Anna");
+// console.log(JSON.stringify(manuel)+ '\nAnd here\'s the array' +JSON.stringify(personalData));
 
-const beatriceIndex = personalData.findIndex((person, idx, persons) => {
-  return person.name === "Beatrice";
-});
+// const beatriceIndex = personalData.findIndex((person, idx, persons) => {
+//   return person.name === "Beatrice";
+// });
 
-console.log(beatriceIndex);
+// console.log(beatriceIndex);
 
-console.log('Does this array include a name equal to Beatrice: '+ personalData.includes('Beatrice'));
+// console.log('Does this array include a name equal to Beatrice: '+ personalData.includes('Beatrice'));
 
-console.log("Here is what's inside the array: " + JSON.stringify(personalData));
+// console.log("Here is what's inside the array: " + JSON.stringify(personalData));
 
-console.log('New Stuff'); // New Stuff
-/* const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-const taxAdjustedPrices = [];
+console.log("New Stuff"); // New Stuff
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
 
-// for (const price of prices) {
-//     taxAdjustedPrices.push(price * (1 + tax));
+// // for (const price of prices) {
+// //     taxAdjustedPrices.push(price * (1 + tax));
 
-prices.forEach((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-    taxAdjustedPrices.push(priceObj);
-});
-console.log(JSON.stringify(taxAdjustedPrices)); */
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//     taxAdjustedPrices.push(priceObj);
+// });
+// console.log(JSON.stringify(taxAdjustedPrices));
 
-/* const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const taxAdjPrices = prices.map((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-    return priceObj;
-});
-console.log(JSON.stringify(taxAdjPrices)); */
+// const taxAdjPrices = prices.map((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   return priceObj;
+// });
+// console.log(JSON.stringify(taxAdjPrices));
 
+// // console.log(sum);
+
+// const sum = prices.reduce((prevValue, curValue, curIndex, prices) => {
+//   return prevValue + curValue;
+// }, 0); // 0 is the initial value of prevValue
+
+// console.log(sum);
+
+// const data = "new york;10.99;2000";
+
+// const transformedData = data.split(";");
+// transformedData[1] = +transformedData[1];
+// console.log(transformedData);
+
+// const nameFragments = ["Max", "Schwarz"];
+// const name = nameFragments.join(";");
+// console.log(name);
+
+// const copiedNameFragments = [...nameFragments];
+
+// nameFragments.push("Mr");
+// console.log(nameFragments, copiedNameFragments);
+
+// console.log(Math.min(...prices));
+
+// const persons = [
+//   { name: "Max", age: 30 },
+//   { name: "Manuel", age: 31 },
+// ];
+// const copiedPersons = persons.map((person) => ({ 
+//     name: person.name, 
+//     age: person.age 
+// }));
+
+// persons.push({ name: "Anna", age: 29 });
+// persons[0].age = 31;
+// persons[2].age = 43;
+// console.log(persons, copiedPersons);
+
+
+const nameData = ["Max", "Schwarz"];
+// const firstName = nameFragments[0];
+// const lastName = nameFragments[1];
+
+// const [firstName, lastName] = nameData;
+// console.log(firstName, lastName);
