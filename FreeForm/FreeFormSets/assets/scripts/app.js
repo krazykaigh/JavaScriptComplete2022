06fa13ebbea7434913e3ab0e5082155f25da8b0e
Kaigh Taylor <coders@kai-deas.com>
2022-12-01 08:32:04 -0500

    JSCG | Sets and Maps Freeform

--- FreeForm/FreeFormSets/assets/scripts/app.js
+++ FreeForm/FreeFormSets/assets/scripts/app.js
@@ -1,1 +1,9 @@
 /*jshint esversion: 8 */
+const ids = new Set(['Hi','From', 'Set']);
+ids.add(2);
+ids.delete('Hi');
+console.log(ids.has(2));
+console.log(ids);
+for (const entry of ids.entries()) {
+  console.log(entry[0]);
+}
