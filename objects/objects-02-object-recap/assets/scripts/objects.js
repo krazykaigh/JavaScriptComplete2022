bb2fe1f5bac4a0b49f2cfed2d2c5e5fe0c60afb0
Kaigh Taylor <coders@kai-deas.com>
2022-12-08 14:36:14 -0500

    jscg | added objects modded files

--- objects/objects-02-object-recap/assets/scripts/objects.js
+++ objects/objects-02-object-recap/assets/scripts/objects.js
@@ -1,10 +1,23 @@
+const movieList= document.getElementById('movie-list');
+
+movieList.style.backgroundColor = 'red';
+movieList.style['background-color'] = 'gray';
+
+movieList.style.display = 'block';
+
 const person = {
   name: 'Max',
+  'last name': 'Mustermann',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
   greet: function() {
     alert('Hi there!');
-  }
+  },
+  1.5: 'hello'
 };
 
-person.greet();
+// person.greet();
+console.log (person['last name']);
+console.log (person[1.5]);
+
+console.log(person);
