80ddf7bbc18b1028b1a3329f1b110522e5a791f4
Kaigh Taylor <coders@kai-deas.com>
2022-12-28 10:04:51 -0500

    jscg | object project

--- objects/objects_project_starter/assets/scripts/app.js
+++ objects/objects_project_starter/assets/scripts/app.js
@@ -4,7 +4,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-function renderMovies() {
+function renderMovies(filter = '') {
   const movieList = document.getElementById('movie-list');
   if (movies.length === 0) {
     movieList.classList.remove('visible');
@@ -13,18 +13,22 @@ function renderMovies() {
     movieList.classList.add('visible');
   }
   movieList.innerHTML = '';
-  movies.forEach((movie) => {
+
+  const filteredMovies = !filter ? movies 
+  : movies.filter(movie => movie.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
     // movieEl.textContent = movie.info.title;
     let text = movie.info.title + ' - ';
     for (const key in movie.info) {
-      if (key != 'title'){
+      if (key != 'title') {
         text = text + `${key}: ${movie.info[key]}`;
+      }
     }
-  }
-  movieEl.textContent = text;
-  movieList.append(movieEl);
-  console.log('Text = ' + text);
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+    console.log('Text = ' + text);
   });
 }
 
@@ -37,21 +41,27 @@ function addMovieHandler() {
     title.trim === '' ||
     extraName === '' ||
     extraValue === ''
-) {
-  return;
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  renderMovies();
+  console.log(newMovie);
 }
 
-const newMovie = {
-  info:{
-    title,
-    [extraName]: extraValue
-  },
-  id: Math.random()
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
 };
 
-movies.push(newMovie);
-renderMovies();
-console.log(newMovie);
-}
-
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
